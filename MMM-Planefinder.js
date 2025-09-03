32c4cde55c8b97a67ed96e322e4db749b8107a83
kds54 <167254682+kds54@users.noreply.github.com>
2025-09-02 20:00:28 -0400

    Update MMM-Planefinder.js

--- MMM-Planefinder.js
+++ MMM-Planefinder.js
@@ -5,7 +5,7 @@
 Module.register("MMM-Planefinder", {
     // Default module config
     defaults: {
-        planefinderIP: "192.168.1.146",
+        planefinderIP: "192.168.1.1",
         shareCode: "", // Your share code if needed
         updateInterval: 30000, // 30 seconds
         maxFlights: 10,
